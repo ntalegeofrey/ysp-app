a2c02cabf5b1113dd7155df8422d9515c7ba4c32
ntalegeofrey <ntalegeofrey@gmail.com>
2025-11-03 18:26:58 -0500

    Frontend test2

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -4,6 +4,8 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <h1>Hello YSP</h1>
+      <p>Welcome to the YSP App</p>
+      <p>Version 1.0.0</p>
     </div>
   );
 }
